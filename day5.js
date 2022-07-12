76298a544ba214bb7275f52ab290d6f0bd2aac2e
Bhavika <jnparulster@gmail.com>
2022-07-12 21:18:36 +0530

    function

---
+++ day5.js
@@ -0,0 +1,42 @@
+// // 'snjckdsm"dmkmc' - String
+// // ["apple","mango"]
+// // const fruit={[mango]:"m"}
+// console.log ('connected')
+
+// let user= 'Bhavika'
+// console.log(user)
+// user= 'Rajesh'
+// console.log(user)
+// console.log= (2+4)
+// let number1 = ['a','b','c','d','e']
+// maggi,// console.log(number1.split[','])                                                      
+// function cookmaggi( water, pan){
+//     console.log('your ' + maggi + 'blocks of maggi, made in'
+//      + water + 'litres of water in ' + pan + ' pans is ready. ' )
+
+//      return 'You can now get other orders'
+// }
+
+// function randomNumber(){
+//     console.log(Math.ceil() * 10 ) 
+// }
+
+// let number= randomNumber()
+// console.log(number)
+
+// let result = cookmaggi(2, 2, 2)
+// console.log(result)
+
+// // (Math.ciel)
+// function addtwonum(a,b){
+//     return a+b
+// }
+
+// let sumtwonum = addtwonum(11,6)
+// console.log(sumtwonum)
+
+for(let i = 6; i <= 60 ; i= i+6 )
+console.log(i)
+
+ for(let s =10; s > 1 ; s= s /5 )
+ console.log(s)
